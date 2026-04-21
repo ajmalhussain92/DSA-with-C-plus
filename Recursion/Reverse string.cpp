dd9b0fd57152005024d4961cb1d7050d33faba45
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-04-21 16:07:16 +0530

    Code improved

--- Recursion/Reverse string.cpp
+++ Recursion/Reverse string.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-string rev_str (string str) {
+string revStr (string s) {
     // base case
-    if (str.empty()) return ""; 
+    if (s.empty()) return "";
     
-    return rev_str (str.substr (1)) + str[0];
+    return revStr (s.substr (1)) + s[0];
 }
 
 int main () {
@@ -15,3 +15,12 @@ int main () {
 	
 	return 0;
 }
+
+/*
+
+substr (0) means Remove substring from index 0 to 0		| "ABC".substr (0) = ABC
+substr (1) means Remove substring from index 0 to 1 	| "ABC".substr (1) = BC
+substr (2) means Remove substring from index 0 to 2 	| "ABC".substr (2) = C
+substr (3) means Remove substring from index 0 to 3 	| "ABC".substr (3) = ""
+
+*/
